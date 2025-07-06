89180e70d4fe92e7e505e3c92e9ffccfca7c5f3a
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-06 17:26:56 +0530

    Add files via upload

---
+++ learn-java-programs/dowhileexample.java
@@ -0,0 +1,10 @@
+public class dowhileexample{
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Using do-while loop:");
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
