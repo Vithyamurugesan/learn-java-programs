929b938cfd6c622f7ff954a924fe9d8801f4e56d
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-06 17:25:16 +0530

    Add files via upload

---
+++ learn-java-programs/forexample.java
@@ -0,0 +1,8 @@
+public class forexample {
+    public static void main(String[] args) {
+        System.out.println("Using for loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
