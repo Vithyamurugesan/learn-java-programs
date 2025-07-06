f949f501795859991248aa75a83e8bb79ecb3b66
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-06 17:26:08 +0530

    Add files via upload

---
+++ learn-java-programs/whileexample.java
@@ -0,0 +1,10 @@
+public class whileexample {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Using while loop:");
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
