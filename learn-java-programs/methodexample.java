8099bb270a585cfdcdac20b00024cda41d32cee8
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-11 11:42:19 +0530

    Add files via upload

---
+++ learn-java-programs/methodexample.java
@@ -0,0 +1,11 @@
+public class methodexample {
+    public void printmsg(){
+        System.out.println("Hello,I am Vithya.");
+    }
+    public static void main(String [] args){
+        methodexample obj=new methodexample();
+        obj.printmsg();
+
+    }
+    
+}
