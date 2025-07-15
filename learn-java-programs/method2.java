072686a7aa71d953a3f0104c8f6df592c0969a54
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-15 10:11:32 +0530

    Add files via upload

---
+++ learn-java-programs/method2.java
@@ -0,0 +1,12 @@
+public class method2 {
+    // Method without return and with arguments
+    void displayMessage(String name) {
+        System.out.println("Hello, " + name + "! Have a great day.");
+    }
+
+    public static void main(String[] args) {
+        method2 obj = new method2();
+        obj.displayMessage("Vithya");
+    }
+}
+
