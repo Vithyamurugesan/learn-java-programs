03e25555402454e0882d8e52beb60a899e5d27c7
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-15 10:06:17 +0530

    Add files via upload

---
+++ learn-java-programs/method1.java
@@ -0,0 +1,15 @@
+//without return and without argument
+public class method1 {
+   // Method without return and without arguments
+    void greet() {
+        System.out.println("Hello! Welcome to Java.");
+    }
+
+    public static void main(String[] args) {
+        method1 obj = new method1();
+        obj.greet();
+    }
+}
+
+    
+
