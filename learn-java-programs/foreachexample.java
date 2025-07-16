70b97df86f6e36f6c09dcf171a657ed45b057617
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-16 22:20:15 +0530

    Add files via upload

---
+++ learn-java-programs/foreachexample.java
@@ -0,0 +1,15 @@
+public class foreachexample {
+    public static void main(String [] args){
+        int [] num={12,13,14,15};
+        for(int var:num){
+            System.out.println(var);
+        }
+        System.out.println("\n");
+
+        String[] num2={"one","two"};
+        for(String var2:num2){
+            System.out.println(var2);
+        }
+    }
+    
+}
