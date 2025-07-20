7037328a5acab524d805c860b2b8e8ae29ff6a92
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:25:07 +0530

    Add files via upload

---
+++ learn-java-programs/strequalsexample.java
@@ -0,0 +1,6 @@
+public class strequalsexample{
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Equals 'Hello'? " + str.equals("Hello"));  // Output: false
+    }
+}
