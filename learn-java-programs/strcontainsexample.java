b4641d874988da9b1474c8e91327283befaf1f0f
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:22:55 +0530

    Add files via upload

---
+++ learn-java-programs/strcontainsexample.java
@@ -0,0 +1,6 @@
+public class strcontainsexample {
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Contains 'Java'? " + str.contains("Java"));  // Output: true
+    }
+}
