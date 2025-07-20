ffe99af6348ac4ccd60df318c199230707966e19
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:15:37 +0530

    Add files via upload

---
+++ learn-java-programs/StringLengthExample.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class StringLengthExample{
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Length: " + str.length());  // Output: 20
+    }
+}
