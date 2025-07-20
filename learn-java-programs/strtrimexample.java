8ecdc5369c3704bb7a97ad990bb34c46396315fc
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:39:38 +0530

    Add files via upload

---
+++ learn-java-programs/strtrimexample.java
@@ -0,0 +1,7 @@
+public class strtrimexample {
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Trimmed: '" + str.trim() + "'");  // Output: "Hello Java World"
+    }
+    
+}
