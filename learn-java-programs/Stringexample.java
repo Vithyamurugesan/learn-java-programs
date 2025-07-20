b638f359b41d888e1caf71a52f17abfdb843ed84
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:15:14 +0530

    Add files via upload

---
+++ learn-java-programs/Stringexample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+    public class Stringexample{
+        public static void main(String[]args){
+            String s = "Hello";
+            s.concat(" World");
+            System.out.println(s);
+        }
+
+    }
+
