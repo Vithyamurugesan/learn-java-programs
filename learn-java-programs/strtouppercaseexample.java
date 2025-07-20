0e02611ca3e03d77fb48c18b4240835932fbd784
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:38:33 +0530

    Add files via upload

---
+++ learn-java-programs/strtouppercaseexample.java
@@ -0,0 +1,7 @@
+public class strtouppercaseexample {
+     public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Uppercase: " + str.toUpperCase());  // Output: "  HELLO JAVA WORLD  "
+    }
+    
+}
