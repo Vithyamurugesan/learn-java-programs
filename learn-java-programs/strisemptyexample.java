cf0fbfdbe063bb28022dc9364e83d4cc91bdf571
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:42:10 +0530

    Add files via upload

---
+++ learn-java-programs/strisemptyexample.java
@@ -0,0 +1,7 @@
+public class strisemptyexample {
+     public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Is string empty? " + str.isEmpty());  // false
+    }
+    
+}
