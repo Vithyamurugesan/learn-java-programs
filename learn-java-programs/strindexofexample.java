8e149204cf63c3ecb3c1d0c9713c3a5cef37bf5d
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:40:45 +0530

    Add files via upload

---
+++ learn-java-programs/strindexofexample.java
@@ -0,0 +1,8 @@
+public class strindexofexample {
+     public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Index of 'Java': " + str.indexOf("Java"));     // Output: 8
+        System.out.println("Last index of 'o': " + str.lastIndexOf("o"));  // Output: 14
+    }
+    
+}
