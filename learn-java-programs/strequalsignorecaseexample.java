6876584c15780df26daaefcb8477c8471b2b9edf
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:27:06 +0530

    Add files via upload

---
+++ learn-java-programs/strequalsignorecaseexample.java
@@ -0,0 +1,8 @@
+public class strequalsignorecaseexample{
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("EqualsIgnoreCase '  hello java world  '? " + 
+                            str.equalsIgnoreCase("  hello java world  ")); // Output: true
+    }
+}
+
