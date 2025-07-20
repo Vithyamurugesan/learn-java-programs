a05fcd0ab81172d3d51fba34f630df5d3afdab6f
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:42:40 +0530

    Add files via upload

---
+++ learn-java-programs/strvalueofexample.java
@@ -0,0 +1,8 @@
+public class strvalueofexample {
+     public static void main(String[] args) {
+        int num = 100;
+        String numStr = String.valueOf(num);
+        System.out.println("ValueOf example: " + numStr + " is now a string.");
+    }
+    
+}
