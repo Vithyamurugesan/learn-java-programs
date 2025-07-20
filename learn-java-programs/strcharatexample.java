9f2d03eb44e1ac5869c41ca09890964b1ae7b534
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:22:32 +0530

    Add files via upload

---
+++ learn-java-programs/strcharatexample.java
@@ -0,0 +1,6 @@
+public class strcharatexample {
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Character at index 2: " + str.charAt(2));  // Output: H
+    }
+}
