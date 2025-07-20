0abf5d36e00623c170a0ae2edea96d135ffa5fcb
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:04:35 +0530

    Add files via upload

---
+++ learn-java-programs/Interfaceexample.java
@@ -0,0 +1,21 @@
+    interface Animal {
+             void sound();  
+        }
+        class Dog implements Animal {
+            public void sound() {
+                System.out.println("Bark");
+    }
+}
+        class Cat implements Animal {
+             public void sound() {
+                 System.out.println("Meow");
+             }
+            }
+            public class Interfaceexample {
+                public static void main(String[] args) {
+                    Animal dog = new Dog();
+                    dog.sound();  
+                    Animal cat = new Cat();
+                    cat.sound();  
+    }
+}
