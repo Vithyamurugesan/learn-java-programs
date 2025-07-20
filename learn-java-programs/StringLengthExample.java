7cdc1f024e207adaaa0fe7558e3a3573df229b21
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:16:14 +0530

    Delete learn-java-programs/StringLengthExample.java

--- learn-java-programs/StringLengthExample.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class StringLengthExample{
-    public static void main(String[] args) {
-        String str = "  Hello Java World  ";
-        System.out.println("Length: " + str.length());  // Output: 20
-    }
-}
