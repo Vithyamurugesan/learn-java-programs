79e12e5a7bb9a60b671a523f3874211874fcfed7
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:41:10 +0530

    Add files via upload

---
+++ learn-java-programs/strreplaceexample.java
@@ -0,0 +1,7 @@
+public class strreplaceexample {
+     public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Replace 'Java' with 'Python': " + str.replace("Java", "Python"));
+    }
+    
+}
