e8c83d64a8d2de1724c17577154fadf180f0c68b
Vithyamurugesan <vithyabtech54@gmail.com>
2025-07-20 23:39:04 +0530

    Add files via upload

---
+++ learn-java-programs/strtolowercaseexample.java
@@ -0,0 +1,7 @@
+public class strtolowercaseexample {
+    public static void main(String[] args) {
+        String str = "  Hello Java World  ";
+        System.out.println("Lowercase: " + str.toLowerCase());  // Output: "  hello java world  "
+    }
+    
+}
